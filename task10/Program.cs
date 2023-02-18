0e4c2aed353b87ad5bc7bd2e7e303b65886e88bd
Nikita Krivosheev <nikita47598@mail.ru>
2023-02-18 13:29:12 +0300

    task10

--- task10/Program.cs
+++ task10/Program.cs
@@ -3,3 +3,17 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.WriteLine("Введите число:");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Mult(int num)
+{
+    int firstNum = num / 10;
+    int secondNum = firstNum % 10;
+    return secondNum;
+}
+
+int result = Mult(number);
+Console.WriteLine($"{number} -> {result}");
