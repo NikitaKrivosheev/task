41e6d3c24444ab7cd048921f1b75a3fc20274c22
Nikita Krivosheev <nikita47598@mail.ru>
2023-03-03 14:39:29 +0300

    task35-37

---
+++ task35/Program.cs
@@ -0,0 +1,48 @@
+﻿// 35. Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10, 99].
+// Пример из 5 элементов
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+int[] ArrayRnd(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("Элементы в массиве [");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else
+            Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int TwoDigitNumbers(int[] arr)
+{
+    int sumTwoDigit = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99)
+            sumTwoDigit++;
+    }
+    return sumTwoDigit;
+}
+
+int[] array = ArrayRnd(123, 1, 300);
+PrintArray(array);
+int twoDigit = TwoDigitNumbers(array);
+Console.WriteLine($"В массиве {twoDigit} двузначных чисел");
