b31f07c585aac814d7804bead20d6f378cca397d
Nikita Krivosheev <nikita47598@mail.ru>
2023-03-10 18:58:09 +0300

    task49-50-52

---
+++ task50/Program.cs
@@ -0,0 +1,73 @@
+﻿// 50. Напишите программу, которая на вход
+// принимает позиции элемента в двумерном массиве, и
+// возращает значиние этого элемента или же указание,
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1,7 -> такого элемента в массиве нет
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+bool PositionElement(int[,] matrix, int pos, int pos2)
+{
+    if (pos < matrix.GetLength(0) && pos2 < matrix.GetLength(1))
+    {
+        Console.WriteLine($"Элемент в двухмерном массиве: {matrix[pos, pos2]}");
+        return true;
+    }
+    else
+    {
+        Console.WriteLine($"{pos},{pos2} такого элемента в массиве нет.");
+        return false;
+    }
+}
+
+int[,] matrixRnd = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(matrixRnd);
+
+Console.WriteLine($"Введите позиции элемента в двумерном массиве: ");
+int positionRow = Convert.ToInt32(Console.ReadLine());
+int positionColumn = Convert.ToInt32(Console.ReadLine());
+PositionElement(matrixRnd, positionRow, positionColumn);
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------На всякий случай
+
+// bool PositionElement(int[,] matrix, int pos, int pos2)
+// {
+//     if (pos < matrix.GetLength(0) && pos2 < matrix.GetLength(1))
+//         return true;
+//     else
+//         return false;
+// }
+
+// if (PositionElement(matrixRnd, positionRow, positionColumn))
+//     Console.WriteLine($"Элемент в двухмерном массиве: {matrixRnd[positionRow, positionColumn]}");
+// else
+//     Console.WriteLine($"{positionRow},{positionColumn} такого элемента в массиве нет.");
---
+++ task52/Program.cs
@@ -0,0 +1,65 @@
+﻿// 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4 
+// Среднее арифметическое кафдого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+double[] AverageMatrix(int[,] matrix)
+{
+    double[] arr = new double[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum = sum + matrix[i, j];
+        }
+        arr[j] = sum / matrix.GetLength(0);
+    }
+    return arr;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void PrintAverageMatrix(double[] arr)
+{
+    Console.Write("|");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{Math.Round(arr[i], 1),5}");
+    }
+    Console.WriteLine("|");
+}
+
+int[,] matrixRnd = CreateMatrixRndInt(3, 4, 1, 10);
+double[] averageMatrix = AverageMatrix(matrixRnd);
+PrintMatrix(matrixRnd);
+Console.WriteLine("Среднее фрифметическое каждого столбца:");
+PrintAverageMatrix(averageMatrix);
