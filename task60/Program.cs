3de4b696df6c599281618a35648a10ab441bf0d0
Nikita Krivosheev <nikita47598@mail.ru>
2023-03-15 03:08:14 +0300

    task60

--- task60/Program.cs
+++ task60/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 60. Сформируйте трёхмерный массив из неповторяющихся
+// двузначных чисел. Напишите программу, которая будет построчно выводить
+// массив, добавляя индексы каждого элемента.
+
+int[,,] CreateMatrixInt(int rows, int columns, int depth, int count)
+{
+    int[,,] matrix = new int[rows, columns, depth];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int k = 0; k < matrix.GetLength(2); k++)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                matrix[i, j, k] += count;
+                count += 1;
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int k = 0; k < matrix.GetLength(2); k++)
+        {
+            Console.Write("|");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.Write($"{matrix[i, j, k],5} ({i},{j},{k})");
+            }
+            Console.WriteLine("|");
+        }
+    }
+}
+
+bool CheckMinMax(int min, int max)
+{
+    if (min >= 10
+        && max <= 99
+        && max > min
+        && min < max)
+        return true;
+    else
+        return false;
+}
+
+int from = 50;
+int before = 99;
+int[,,] matrixInt = CreateMatrixInt(3, 3, 3, from);
+if (CheckMinMax(from, before))
+    PrintMatrix(matrixInt);
+else
+    Console.WriteLine("Ошибка ввода данных.");
+
+
