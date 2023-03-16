a623bdead28afb0cfcfc66d63a76885c297dd052
Nikita Krivosheev <nikita47598@mail.ru>
2023-03-17 00:07:11 +0300

    task68

--- task68/Program.cs
+++ task68/Program.cs
@@ -7,14 +7,14 @@ int numberM = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число n: ");
 int numberN = Convert.ToInt32(Console.ReadLine());
 
-int AckermanFunction(int m, int n)
+int AckermanFunc(int m, int n)
 {
     if (m == 0) return n + 1;
-    else if (n == 0) return AckermanFunction(m - 1, 1);
-    else return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+    if (n == 0) return AckermanFunc(m - 1, 1);
+    return AckermanFunc(m - 1, AckermanFunc(m, n - 1));
 }
 
-int result = AckermanFunction(numberM, numberN);
+int result = AckermanFunc(numberM, numberN);
 if (numberM < 0 && numberN < 0)
     Console.WriteLine($"Ошибка ввода данных.");
 else
